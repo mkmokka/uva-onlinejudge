e2af4a755106afdbc54750f3f507bbdece10f204
mkmokka <mkmokka12@gmail.com>
2026-06-11 15:14:52 +0600

    Add files via upload

--- 10222.cpp
+++ 10222.cpp
@@ -1,44 +1,49 @@
-/*
-10222 - Decode the Mad man
-*/
+/*******************************************************
+ * Author: Nguyen Truong Duy
+ *******************************************************/
 
-#include<bits/stdc++.h>
+/**
+ * Following are the assumptions we can make to pass the test cases:
+ *  - All alpha-characters are lowercase
+ *  - No 'shift-characters' are present (such as !, @, {, }, ?)
+ */
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+
+#define MAX_LEN 10000
+
 using namespace std;
-int main()
-{
-    int i,j,len;
-    char text[1000],n;
-    char ar[]="`1234567890-=qwertyuiop[]asdfghjkl;'\\zxcvbnm,./";
-    gets(text);
-    len=strlen(text);
-    for(i=0; i<len; i++)
-    {
-        n=tolower(text[i]);
-        if(n!='`'&& n!='q'&&n!='a'&&n!='z'
-                &&n!='w'&&n!='s'&&n!='x')
-        {
-            for(j=0; j<=46; j++)
-            {
-                if(n==' ')
-                {
-                    printf(" ");
-                    break;
-                }
-                if(n==ar[j])
-                {
-                    printf("%c",ar[j-2]);
-                    break;
-                }
-            }
-        }
-    }
-    cout<<endl;
-    return 0;
-}
-/*
-Sample Input
-k[r dyt I[o
 
-Sample Output
-how are you
-*/
+char decodeCharacter(char character, const vector<string>& keyboard) {
+  for (int i = 0; i < keyboard.size(); ++i) {
+    int charInd = keyboard[i].find(character);
+    if (charInd != string::npos && charInd >= 2) {
+      return keyboard[i][charInd - 2];
+    }
+  }
+  return character;
+}
+
+int main(void) {
+  vector<string> keyboard;
+  keyboard.push_back("1234567890-=");
+  keyboard.push_back("qwertyuiop[]\\");
+  keyboard.push_back("asdfghjkl;'");
+  keyboard.push_back("zxcvbnm,./");
+
+  char encodedStr[MAX_LEN + 1];
+  gets(encodedStr);
+  
+  char decodedStr[MAX_LEN + 1];
+  int len = 0;
+  for (len = 0; encodedStr[len] != '\0'; ++len) {
+    decodedStr[len] = decodeCharacter(encodedStr[len], keyboard);
+  }
+  decodedStr[len] = '\0';
+  printf("%s\n", decodedStr);
+       
+  return 0;
+}
